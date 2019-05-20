29509d64138ac4370ec9ae3631dc2b8f39596428
Gisela Pinto <gii-pinto@hotmail.com>
2019-05-20 16:18:32 +0100

    Change mv to cp on local script

--- Project/scriptLocal.sh
+++ Project/scriptLocal.sh
@@ -23,62 +23,62 @@ echo -e "\n${bold}* Compilação do código em cada nó *${normal}"
 
 echo -e "\n${bold}->${normal} A compilar Registry"
 javac Registry/src/Interfaces/*.java Registry/src/MainPackage/*.java
-mv Registry/src/Interfaces/*.class Registry/src/dir_registry/Interfaces/
-mv Registry/src/MainPackage/*.class Registry/src/dir_registry/MainPackage/
+cp Registry/src/Interfaces/*.class Registry/src/dir_registry/Interfaces/
+cp Registry/src/MainPackage/*.class Registry/src/dir_registry/MainPackage/
 
 
 echo -e "\n${bold}->${normal} A compilar Manager"
 javac Manager/src/Interfaces/*.java Manager/src/MainPackage/*.java Manager/src/EntitiesState/*.java
-mv Manager/src/Interfaces/*.class Manager/src/dir_manager/Interfaces/
-mv Manager/src/MainPackage/*.class Manager/src/dir_manager/MainPackage/
-mv Manager/src/EntitiesState/*.class Manager/src/dir_manager/EntitiesState/
+cp Manager/src/Interfaces/*.class Manager/src/dir_manager/Interfaces/
+cp Manager/src/MainPackage/*.class Manager/src/dir_manager/MainPackage/
+cp Manager/src/EntitiesState/*.class Manager/src/dir_manager/EntitiesState/
 
 echo -e "\n${bold}->${normal} A compilar Customer"
 javac Customer/src/Interfaces/*.java Customer/src/MainPackage/*.java Customer/src/EntitiesState/*.java
-mv Customer/src/Interfaces/*.class Customer/src/dir_customers/Interfaces/
-mv Customer/src/MainPackage/*.class Customer/src/dir_customers/MainPackage/
-mv Customer/src/EntitiesState/*.class Customer/src/dir_customers/EntitiesState/
+cp Customer/src/Interfaces/*.class Customer/src/dir_customers/Interfaces/
+cp Customer/src/MainPackage/*.class Customer/src/dir_customers/MainPackage/
+cp Customer/src/EntitiesState/*.class Customer/src/dir_customers/EntitiesState/
 
 echo -e "\n${bold}->${normal} A compilar Mechanics"
 javac Mechanic/src/Interfaces/*.java Mechanic/src/MainPackage/*.java Mechanic/src/EntitiesState/*.java
-mv Mechanic/src/Interfaces/*.class Mechanic/src/dir_mechanic/Interfaces/
-mv Mechanic/src/MainPackage/*.class Mechanic/src/dir_mechanic/MainPackage/
-mv Mechanic/src/EntitiesState/*.class Mechanic/src/dir_mechanic/EntitiesState/
+cp Mechanic/src/Interfaces/*.class Mechanic/src/dir_mechanic/Interfaces/
+cp Mechanic/src/MainPackage/*.class Mechanic/src/dir_mechanic/MainPackage/
+cp Mechanic/src/EntitiesState/*.class Mechanic/src/dir_mechanic/EntitiesState/
 
 
 echo -e "\n${bold}->${normal} A compilar Lounge"
 javac Lounge/src/Interfaces/*.java Lounge/src/MainPackage/*.java
-mv Lounge/src/Interfaces/*.class Lounge/src/dir_lounge/Interfaces/
-mv Lounge/src/MainPackage/*.class Lounge/src/dir_lounge/MainPackage/
+cp Lounge/src/Interfaces/*.class Lounge/src/dir_lounge/Interfaces/
+cp Lounge/src/MainPackage/*.class Lounge/src/dir_lounge/MainPackage/
 
 
 
 echo -e "\n${bold}->${normal} A compilar Repair Area"
 javac RepairArea/src/Interfaces/*.java RepairArea/src/MainPackage/*.java
-mv RepairArea/src/Interfaces/*.class RepairArea/src/dir_repairArea/Interfaces/
+cp RepairArea/src/Interfaces/*.class RepairArea/src/dir_repairArea/Interfaces/
 mv RepairArea/src/MainPackage/*.class RepairArea/src/dir_repairArea/MainPackage/
 
 
 echo -e "\n${bold}->${normal} A compilar Outside World"
 javac OutsideWorld/src/Interfaces/*.java OutsideWorld/src/MainPackage/*.java
-mv OutsideWorld/src/Interfaces/*.class OutsideWorld/src/dir_outsideWorld/Interfaces/
-mv OutsideWorld/src/MainPackage/*.class OutsideWorld/src/dir_outsideWorld/MainPackage/
+cp OutsideWorld/src/Interfaces/*.class OutsideWorld/src/dir_outsideWorld/Interfaces/
+cp OutsideWorld/src/MainPackage/*.class OutsideWorld/src/dir_outsideWorld/MainPackage/
 
 echo -e "\n${bold}->${normal} A compilar Park"
 javac Park/src/Interfaces/*.java Park/src/MainPackage/*.java
-mv Park/src/Interfaces/*.class Park/src/dir_park/Interfaces/
-mv Park/src/MainPackage/*.class Park/src/dir_park/MainPackage/
+cp Park/src/Interfaces/*.class Park/src/dir_park/Interfaces/
+cp Park/src/MainPackage/*.class Park/src/dir_park/MainPackage/
 
 echo -e "\n${bold}->${normal} A compilar Supplier Site"
 javac SupplierSite/src/Interfaces/*.java SupplierSite/src/MainPackage/*.java
-mv SupplierSite/src/Interfaces/*.class SupplierSite/src/dir_supplierSite/Interfaces/
-mv SupplierSite/src/MainPackage/*.class SupplierSite/src/dir_supplierSite/MainPackage/
+cp SupplierSite/src/Interfaces/*.class SupplierSite/src/dir_supplierSite/Interfaces/
+cp SupplierSite/src/MainPackage/*.class SupplierSite/src/dir_supplierSite/MainPackage/
 
 
 echo -e "\n${bold}->${normal} A compilar Logger"
 javac GeneralInformationRepo/src/Interfaces/*.java GeneralInformationRepo/src/MainPackage/*.java
-mv GeneralInformationRepo/src/Interfaces/*.class GeneralInformationRepo/src/dir_logger/Interfaces/
-mv GeneralInformationRepo/src/MainPackage/*.class GeneralInformationRepo/src/dir_logger/MainPackage/
+cp GeneralInformationRepo/src/Interfaces/*.class GeneralInformationRepo/src/dir_logger/Interfaces/
+cp GeneralInformationRepo/src/MainPackage/*.class GeneralInformationRepo/src/dir_logger/MainPackage/
 
 echo -e "\n${bold}* Execução do código em cada nó *${normal}"
 
